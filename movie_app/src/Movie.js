c0adae1e07a5c223360f58497abb81176b1a450c
eclipse324 <eclipse324@nate.com>
2018-09-04 17:39:27 +0900

    movieApp

---
+++ movie_app/src/Movie.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './movie.css';
+
+class Movie extends Component {
+  render(){
+    return(
+      <h1>Hello</h1>
+    )
+  }
+}
+
+export default Movie;
